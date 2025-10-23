6c2ddf5a7fbda435ca1ceb4367aed80a9b6f9c94
NoorHameeda <shaiknoorhameeda@gmail.com>
2025-10-23 11:04:42 +0530

    week-2(2)aggregatefun.sql
    Aggregate function such as sum, avg, count, max, min are used

---
+++ week-2(2)aggregatefun.sql
@@ -0,0 +1,10 @@
+---Find the average salary of company 
+Select AVG(salary) from company; 
+---Find the Sum of salariesof company
+ Select SUM(salary) fromcompany;
+---Find the Maximum amount of company
+ Select Max(salary) from company; 
+---Find the Minimum amount of company
+ Select Min(salary) from company;
+---Find the number of rows in a company
+ Select Count(*) from company;
